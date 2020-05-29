4b2d74a7dc4ad31c4947384e0fb9d8ed96f4295a
ricky <ricky@tjetak.com>
2020-05-29 21:17:45 +0700

    integrate storage to calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -3,24 +3,35 @@
     <h1>Paper Calculator</h1>
     <table>
       <tr>
+        <td><p>paper type</p></td>
+        <td>
+          <select @change="onChange($event)">
+            <option value="default">custom type</option>
+            <option v-for="p in PriceList" v-bind:key="p.paperType">
+              {{ p.paperType }}
+            </option>
+          </select>
+        </td>
+      </tr>
+      <tr>
+        <td><p>price per kg</p></td>
+        <td><input type="number" v-model.number="mPricePerKg" /></td>
+        <p>IDR</p>
+      </tr>
+      <tr>
         <td><p>grammature</p></td>
-        <td><input type="number" v-model.number="m_grammature" /></td>
+        <td><input type="number" v-model.number="mGrammature" /></td>
         <p>gsm</p>
       </tr>
       <tr>
         <td><p>width</p></td>
-        <td><input type="number" v-model.number="m_width" /></td>
+        <td><input type="number" v-model.number="mWidth" /></td>
         <p>mm</p>
       </tr>
       <tr>
         <td><p>length</p></td>
-        <td><input type="number" v-model.number="m_length" /></td>
+        <td><input type="number" v-model.number="mLength" /></td>
         <p>mm</p>
-      </tr>
-      <tr>
-        <td><p>price per Kg</p></td>
-        <td><input type="number" v-model.number="m_price_per_kg" /></td>
-        <p>IDR</p>
       </tr>
     </table>
 
@@ -32,18 +43,36 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import PriceModel from "@/models/price-model";
 
 @Component
 export default class Calculator extends Vue {
+  get PriceList(): PriceModel[] {
+    return this.$store.state.price;
+  }
   result = 0;
-  m_grammature = 0;
-  m_width = 0;
-  m_length = 0;
-  m_price_per_kg = 0;
+  mGrammature = 0;
+  mWidth = 0;
+  mLength = 0;
+  mPricePerKg = 0;
   runCalculation(): void {
     this.result =
-      (this.m_grammature * this.m_width * this.m_length * this.m_price_per_kg) /
+      (this.mGrammature * this.mWidth * this.mLength * this.mPricePerKg) /
       Math.pow(10, 9);
+  }
+  onChange(event: Event): void {
+    const paperType = (event.target as HTMLInputElement).value;
+    if (paperType === "default") {
+      this.mPricePerKg = 0;
+      return;
+    }
+    let priceList = Array<PriceModel>();
+    priceList = this.$store.state.price;
+    priceList.forEach(p => {
+      if (p.paperType == paperType) {
+        this.mPricePerKg = p.pricePerKg;
+      }
+    });
   }
 }
 </script>
