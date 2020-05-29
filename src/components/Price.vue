39131ae40d2765765c9e017029a7dfc36a10b23f
ricky <ricky@tjetak.com>
2020-05-29 18:29:14 +0700

    add local storage

--- src/components/Price.vue
+++ src/components/Price.vue
@@ -12,6 +12,44 @@
       </tr>
     </table>
 
-    <button>save</button>
+    <button @click="save">save</button>
+    <button @click="deleteAll">delete all</button>
+
+    <table border="1">
+      <thead>
+        <th>Paper Type</th>
+        <th>Price per Kg</th>
+      </thead>
+      <tbody>
+        <tr v-for="p in priceList" v-bind:key="p.paperType">
+          <td>{{ p.paperType }}</td>
+          <td>{{ p.pricePerKg }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import PriceModel from "@/models/price-model";
+
+@Component
+export default class Price extends Vue {
+  priceList = Array<PriceModel>();
+
+  public m_paper_type = "";
+  public m_price_per_kg = 0;
+
+  save(): void {
+    this.priceList.push({
+      paperType: this.m_paper_type,
+      pricePerKg: this.m_price_per_kg
+    });
+  }
+
+  deleteAll(): void {
+    this.priceList = [];
+  }
+}
+</script>
